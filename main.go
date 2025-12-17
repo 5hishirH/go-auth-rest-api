afa7a13b2328e9016128f989c0ef4e38a9f3fa04
Shishir Halder <shishirhalder2001@gmail.com>
2025-12-17 10:45:11 +0600

    feat: setup basic router and server

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/5hishirH/go-auth-rest-api.git/config"
+)
 
 func main() {
-	fmt.Println("Auth rest api")
+	// config setup
+	// database setup
+	// router setup
+	// setup server
+
+	cfg := config.MustLoad()
+
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Server is running"))
+	})
+
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	fmt.Println("Server started")
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatal("Failed to start server")
+	}
+
 }
