e3b1e52fe76beb16ddcb9c574d3033dfd0632e88
Shishir Halder <shishirhalder2001@gmail.com>
2025-12-31 11:01:20 +0600

    feat: add role to user model

--- internal/user/model.go
+++ internal/user/model.go
@@ -2,21 +2,21 @@ package user
 
 import "time"
 
-type User struct {
-	Id                      int64     // `json:"id"`
-	Email                   string    // `json:"email" validate:"required"`
-	PasswordHash            string    // `json:"password_hash" validate:"required"`
-	FullName                string    // `json:"full_name" validate:"required"`
-	ProfilePicName          string    // `json:"profile_pic_path" validate:"required"`
-	RefreshToken            string    // `json:"-"`
-	refreshTokenExipryInSec time.Time // `json:"-"`
-	CreatedAt               time.Time //`json:"-"`
-}
+type UserRole string
 
-type RefreshToken struct {
-	Id          int64
-	UserId      int64
-	HashedToken string
-	TokenExpiry time.Time
-	CreatedAt   time.Time
+const (
+	RoleUser  UserRole = "user"
+	RoleAdmin UserRole = "admin"
+)
+
+type User struct {
+	Id                 int64
+	Email              string
+	PasswordHash       string
+	Role               UserRole
+	FullName           string
+	ProfilePicName     string
+	RefreshToken       string
+	RefreshTokenExpiry time.Time
+	CreatedAt          time.Time
 }
